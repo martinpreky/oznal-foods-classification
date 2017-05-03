60000f03598403fc7d682101781d229a64e6f4f3
Miroslav Rac <miroslav.rac@adbee.sk>
2017-05-03 14:15:20 +0200

    pridane veci

--- README.md
+++ README.md
@@ -5,3 +5,23 @@ whl-libraries:
 numpy-1.12.1+mkl-cp35-cp35m-win32.whl
 
 scipy-0.19.0-cp35-cp35m-win32.whl
+
+
+true [1, 1, 1, 0, 1, 1, 1, 0, 1, 0]
+pred [1, 1, 1, 0, 1, 1, 1, 1, 1, 0]
+
+pre 1:
+    tp=7
+    fp=1
+    fn=0
+
+precision = 7/8 = .875
+recall = 7/7 = 1
+
+pre 0:
+    tp=2
+    fp=0
+    fn=1
+
+precision = 2/2 = 1
+recall = 2/3 = .66666667
--- main.py
+++ main.py
@@ -1,14 +1,27 @@
+import numpy as np
 from sklearn.neural_network import MLPClassifier
 import sampleData
 import csvData
 import dbData
 from sklearn.metrics import precision_recall_curve
+from sklearn.metrics.ranking import _binary_clf_curve
 import sys
 
-#hovno = sampleData.get_input_instance()
-#hovno = csvData.get_input_instance()
+# hovno = sampleData.get_input_instance()
+# hovno = csvData.get_input_instance()
 
-for k in range(1,5):
+t=[1, 1, 1, 0, 1, 1, 1, 0, 1, 0]
+p=[1, 1, 1, 0, 1, 1, 1, 1, 1, 0]
+for c in [0, 1]:
+    precision, recall, thresholds = precision_recall_curve(t,
+                                                           p,
+                                                           pos_label=c)
+    print("class " + str(c))
+    print("precision: " + str(precision))
+    print("recall: " + str(recall))
+    print("thresholds: " + str(thresholds))
+sys.exit()
+for k in range(1, 5):
     for b in [True, False]:
         print("")
         print("")
@@ -16,13 +29,14 @@ for k in range(1,5):
         print("kfold: " + str(k))
         print("include recipes: " + str(b))
         sys.stdout.flush()
-        hovno = dbData.get_input_instance('items_kfold_'+str(k), b)
+        hovno = dbData.get_input_instance('items_kfold_' + str(k), b)
 
         X = hovno.train_binary_input
         y = hovno.train_binary_result
 
         print("...Neural network init...")
-        clf = MLPClassifier(solver='adam', alpha=1e-5, hidden_layer_sizes=(10, 2), random_state=1, max_iter=500, verbose=True)
+        clf = MLPClassifier(solver='adam', alpha=1e-5, hidden_layer_sizes=(10, 2), random_state=1, max_iter=500,
+                            verbose=False)
 
         print("...Neural network train...")
         clf.fit(X, y)
@@ -30,10 +44,13 @@ for k in range(1,5):
         print("...Neural network predict...")
         hovno.test_binary_result_predicted = clf.predict(hovno.test_binary_input)
 
-        precision, recall, thresholds = precision_recall_curve(hovno.test_binary_result, hovno.test_binary_result_predicted)
-
-        print(precision)
-        print(recall)
-        print(thresholds)
+        for c in [0, 1]:
+            precision, recall, thresholds = precision_recall_curve(hovno.test_binary_result,
+                                                                   hovno.test_binary_result_predicted,
+                                                                   pos_label=c)
+            print("class " + str(c))
+            print("precision: " + str(precision))
+            print("recall: " + str(recall))
+            print("thresholds: " + str(thresholds))
 
         hovno.message_out()
